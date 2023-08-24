146c34860d6ac3aede3570c6e342771fc625935c
Devin Dow <DevinDow@gmail.com>
2023-08-24 04:37:36 -0700

    moved FacePainter to its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'face_painter.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -96,44 +98,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}
-
-class FacePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 4.0
-      ..style = PaintingStyle.fill
-      ..color = Colors.indigo;
-
-    // Left eye
-    canvas.drawRRect(
-      RRect.fromRectAndRadius(
-          Rect.fromLTWH(20, 40, 100, 100), Radius.circular(20)),
-      paint,
-    );
-    // Right eye
-    canvas.drawOval(
-      Rect.fromLTWH(size.width - 120, 40, 100, 100),
-      paint,
-    );
-    // Mouth
-    final mouth = Path();
-    mouth.moveTo(size.width * 0.8, size.height * 0.6);
-    mouth.arcToPoint(
-      Offset(size.width * 0.2, size.height * 0.6),
-      radius: Radius.circular(150),
-    );
-    mouth.arcToPoint(
-      Offset(size.width * 0.8, size.height * 0.6),
-      radius: Radius.circular(200),
-      clockwise: false,
-    );
-
-    canvas.drawPath(mouth, paint);
-  }
-
-  @override
-  bool shouldRepaint(FaceOutlinePainter oldDelegate) => false;
 }
